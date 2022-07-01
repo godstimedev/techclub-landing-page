1dd220e56543ef8ad4c5ed32db70ccaccc7f630b
Godstime Agholor <agholorgodstime18@gmail.com>
2022-07-01 14:23:50 +0100

    added heading in interactive component

--- src/components/Interactive.jsx
+++ src/components/Interactive.jsx
@@ -4,6 +4,9 @@ function Interactive() {
   return (
     <div className="h-[70vh] w-full bg-[#78B8A4] pt-[5rem]">
       <div className="max-w-[90%]  md:max-w-[1024px] mx-auto">
+        <h1 className="text-center text-3xl font-bold mb-5 text-[]">
+          Interactive Hands-on Learning
+        </h1>
         <iframe
           width="100%"
           height="300"
