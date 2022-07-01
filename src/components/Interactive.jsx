31da9cacfcdd71e1b45a1a10482b1395ba2022d1
Godstime Agholor <agholorgodstime18@gmail.com>
2022-07-01 13:30:13 +0100

    added interactive component

---
+++ src/components/Interactive.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Interactive() {
+  return (
+    <div className="h-[70vh] w-full bg-[#78B8A4] pt-[5rem]">
+      <div className="max-w-[90%]  md:max-w-[1024px] mx-auto">
+        <iframe
+          width="100%"
+          height="300"
+          src="//jsfiddle.net/Sunshine_sunny/8pxvb0yo/1/embedded/html,css,result/dark/"
+          allowfullscreen="allowfullscreen"
+          allowpaymentrequest
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default Interactive;
