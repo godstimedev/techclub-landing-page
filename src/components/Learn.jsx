779c61367b8737a5f2c9e4e8258d08f276b2abe9
Godstime Agholor <agholorgodstime18@gmail.com>
2022-07-01 13:52:02 +0100

    made icons better

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { AiFillHtml5 } from "react-icons/ai";
-import { FaCss3Alt, FaPaintBrush, FaReact } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaPaintBrush,
+  FaLaptop,
+  FaReact,
+} from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
-import { BiDevices } from "react-icons/bi";
+// import { BiDevices } from "react-icons/bi";
 
 function Learn() {
   return (
@@ -10,32 +15,32 @@ function Learn() {
       <div className="bg-white max-w-[1064px] mx-auto  rounded-md px-2 md:px-[3rem] py-[4rem]">
         <h3 className="text-3xl py-3 text-center">What You'll Learn</h3>
         <div className="grid md:grid-cols-2 py-4 text-[#333]">
-          <div className="md:max-w-[90%] py-4 flex gap-[1rem]">
-            <AiFillHtml5 size={30} className="text-[#78B8A4] inline-block" />
-            <div className="md:max-w-[80%]">
+          <div className="md:max-w-[90%] py-4 flex justify-around">
+            <FaHtml5 size={30} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] px-2">
               <h4 className="text-xl font-semibold">HTML</h4>
               <p>Learn the basic building blocks that go into every website.</p>
             </div>
           </div>
-          <div className="md:max-w-[90%] py-4 flex gap-[1rem]">
+          <div className="md:max-w-[90%] py-4 flex justify-around">
             <FaCss3Alt size={30} className="text-[#78B8A4] inline-block" />
-            <div className="md:max-w-[80%]">
+            <div className="w-[85%] px-2">
               <h4 className="text-xl font-semibold">CSS Essentials</h4>
               <p>Learn about the basic styling of every website.</p>
             </div>
           </div>
-          <div className="md:max-w-[90%] py-4 flex gap-[1rem]">
+          <div className="md:max-w-[90%] py-4 flex justify-around">
             <SiJavascript size={30} className="text-[#78B8A4] inline-block" />
-            <div className="md:max-w-[80%]">
+            <div className="w-[85%] px-2">
               <h4 className="text-xl font-semibold">JAVASCRIPT</h4>
               <p>
                 Learn the basics of the web's most popular programming language
               </p>
             </div>
           </div>
-          <div className="md:max-w-[90%] py-4 flex gap-[1rem]">
-            <BiDevices size={30} className="text-[#78B8A4] inline-block" />
-            <div className="md:max-w-[80%]">
+          <div className="md:max-w-[90%] py-4 flex justify-around">
+            <FaLaptop size={30} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] px-2">
               <h4 className="text-xl font-semibold">
                 RESPONSIVE WEB DEVELOPMENT
               </h4>
@@ -45,16 +50,16 @@ function Learn() {
               </p>
             </div>
           </div>
-          <div className="md:max-w-[90%] px-2 py-4 flex gap-[1rem]">
+          <div className="md:max-w-[90%] px-2 py-4 flex justify-around">
             <FaPaintBrush size={25} className="text-[#78B8A4] inline-block" />
-            <div className="md:max-w-[80%]">
+            <div className="w-[85%] px-2">
               <h4 className="text-xl font-semibold">CSS FRAMEWORKS</h4>
               <p>Learn the basic building blocks that go into every website.</p>
             </div>
           </div>
-          <div className="md:max-w-[90%] px-2 py-4 flex gap-[1rem]">
+          <div className="md:max-w-[90%] px-2 py-4 flex justify-around">
             <FaReact size={30} className="text-[#78B8A4] inline-block" />
-            <div className="md:max-w-[80%]">
+            <div className="w-[85%] px-2">
               <h4 className="text-xl font-semibold">REACT JS</h4>
               <p>Learn the basic building blocks that go into every website.</p>
             </div>
