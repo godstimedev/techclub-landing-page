37bf0c9f050568353db61783add4531e0499c90c
Godstime Agholor <agholorgodstime18@gmail.com>
2022-07-03 22:55:51 +0100

    new changes

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -10,7 +10,7 @@ function Learn() {
         <h3 className="text-3xl font-bold py-3 text-center">
           What You'll Learn
         </h3>
-        <div className="grid md:grid-cols-3 gap-[2rem] py-4 text-[#333]">
+        <div className="grid place-items-center md:grid-cols-3 gap-[2rem] py-4 text-[#333]">
           <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
             <FaHtml5 size={40} className="text-[#78B8A4] inline-block" />
             <div className="w-[85%] pt-2 px-2">
