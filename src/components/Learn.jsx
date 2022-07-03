5e778dbe3e5d9f37a8e80c3d7b197615402f780d
Godstime Agholor <agholorgodstime18@gmail.com>
2022-07-03 22:53:31 +0100

    new changes

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -10,33 +10,33 @@ function Learn() {
         <h3 className="text-3xl font-bold py-3 text-center">
           What You'll Learn
         </h3>
-        <div className="grid md:grid-cols-2 py-4 text-[#333]">
-          <div className="md:max-w-[90%] py-4 flex justify-around">
-            <FaHtml5 size={30} className="text-[#78B8A4] inline-block" />
-            <div className="w-[85%] px-2">
+        <div className="grid md:grid-cols-3 gap-[2rem] py-4 text-[#333]">
+          <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
+            <FaHtml5 size={40} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] pt-2 px-2">
               <h4 className="text-xl font-semibold">HTML</h4>
               <p>Learn the basic building blocks that go into every website.</p>
             </div>
           </div>
-          <div className="md:max-w-[90%] py-4 flex justify-around">
-            <FaCss3Alt size={30} className="text-[#78B8A4] inline-block" />
-            <div className="w-[85%] px-2">
+          <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
+            <FaCss3Alt size={40} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] pt-2 px-2">
               <h4 className="text-xl font-semibold">CSS Essentials</h4>
               <p>Learn about the basic styling of every website.</p>
             </div>
           </div>
-          <div className="md:max-w-[90%] py-4 flex justify-around">
-            <SiJavascript size={30} className="text-[#78B8A4] inline-block" />
-            <div className="w-[85%] px-2">
+          <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
+            <SiJavascript size={40} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] pt-2 px-2">
               <h4 className="text-xl font-semibold">JAVASCRIPT</h4>
               <p>
                 Learn the basics of the web's most popular programming language
               </p>
             </div>
           </div>
-          <div className="md:max-w-[90%] py-4 flex justify-around">
-            <BiDevices size={30} className="text-[#78B8A4] inline-block" />
-            <div className="w-[85%] px-2">
+          <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
+            <BiDevices size={40} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] pt-2 px-2">
               <h4 className="text-xl font-semibold">
                 RESPONSIVE WEB DEVELOPMENT
               </h4>
@@ -46,16 +46,16 @@ function Learn() {
               </p>
             </div>
           </div>
-          <div className="md:max-w-[90%] px-2 py-4 flex justify-around">
-            <FaPaintBrush size={25} className="text-[#78B8A4] inline-block" />
-            <div className="w-[85%] px-2">
+          <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
+            <FaPaintBrush size={35} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] pt-2 px-2">
               <h4 className="text-xl font-semibold">CSS FRAMEWORKS</h4>
               <p>Learn the basic building blocks that go into every website.</p>
             </div>
           </div>
-          <div className="md:max-w-[90%] px-2 py-4 flex justify-around">
-            <FaReact size={30} className="text-[#78B8A4] inline-block" />
-            <div className="w-[85%] px-2">
+          <div className="shadow-lg rounded-2xl w-64 p-4 bg-white flex flex-col items-center text-center">
+            <FaReact size={40} className="text-[#78B8A4] inline-block" />
+            <div className="w-[85%] pt-2 px-2">
               <h4 className="text-xl font-semibold">REACT JS</h4>
               <p>Learn the basic building blocks that go into every website.</p>
             </div>
