86cb647d099e50fea02c0f0ea2eaa92014a43c8c
Sunnny-ojo <Sunny.ojo.14@gmail.com>
2022-07-31 04:57:19 +0100

    changes

--- src/components/Interactive.jsx
+++ src/components/Interactive.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React from 'react';
 
 function Interactive() {
-  return (
-    <div className="min-h-[70vh] w-full bg-gradient-to-b from-[#78B8A4] to-white pt-[3rem]   border-b border-gray-500">
-      <div className="max-w-[90%]  md:max-w-[1024px] mx-auto">
-        <h1 className="text-center text-3xl font-bold mb-[2.5rem] text-[]">
-          Interactive Hands-on Learning
-        </h1>
-        <iframe
-          width="100%"
-          height="300"
-          src="//jsfiddle.net/Sunshine_sunny/8pxvb0yo/1/embedded/html,css,result/dark/"
-          allowfullscreen="allowfullscreen"
-          allowpaymentrequest
-          frameborder="0"
-        ></iframe>
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-h-[70vh] w-full bg-gradient-to-b from-[#78B8A4] to-white pt-[3rem]   border-b border-gray-500">
+			<div className="max-w-[90%]  md:max-w-[1024px] mx-auto">
+				<h1 className="text-center text-3xl font-bold mb-[2.5rem] text-[]">
+					Interactive Hands-on Learning
+				</h1>
+				<iframe
+					title="interactive coding section"
+					width="100%"
+					height="300"
+					src="//jsfiddle.net/Sunshine_sunny/8pxvb0yo/1/embedded/html,css,result/dark/"
+					allowfullscreen="allowfullscreen"
+					allowpaymentrequest
+					frameborder="0"
+				></iframe>
+			</div>
+		</div>
+	);
 }
 
 export default Interactive;
