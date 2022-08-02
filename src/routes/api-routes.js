e653414013cba1c032637a41a259647f5cc5434b
Sunnny-ojo <Sunny.ojo.14@gmail.com>
2022-08-02 17:05:25 +0100

    changes

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -2,3 +2,4 @@ const base_url = 'https://api.rccgtodpteens.org/api';
 
 export const getTestimonialsUrl = `${base_url}/testimonials`;
 export const registerUserUrl = `${base_url}/register`;
+export const contactMessageUrl = `${base_url}/contact-message`;
